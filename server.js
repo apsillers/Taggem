03cd471377007e541cc951261e0b401b3bec506b
Andrew Sillers <apsillers@gmail.com>
2013-11-09 17:51:31 -0500

    Grid bugs, multi-level entity diffs, other stuff

--- server.js
+++ server.js
@@ -263,7 +263,7 @@ io.sockets.on('connection', function (socket) {
         changeListener.emit("change", [level], ['pos']);
     });
     
-    changeListener.emit("change");
+    changeListener.emit("change", [entities[id].z], ["pos", "map"]);
 });
 
 function colorFromId(id) {
